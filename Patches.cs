77f775559c6b6bf565c914e700b765153f9d68ac
Oliver Moloney <127998765+Scrapjon@users.noreply.github.com>
2025-06-05 12:09:36 +1000

    Initial commit

---
+++ Patches.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using HarmonyLib;
+using UnityEngine;
+using BepInEx;
+using BepInEx.Logging;
+using Network;
+using Newtonsoft.Json;
+using Logger = BepInEx.Logging.Logger;
+
+namespace MoloneyCustomCardInjector;
+
+public class Patches
+{
+    [HarmonyPatch(typeof(PlayerPrefClient), nameof(PlayerPrefClient.Save))]
+    [HarmonyPatch(new Type[] { typeof(string), typeof(object) })]
+    [HarmonyPrefix]
+    static bool SaveCustom(ref Task __result, string key, object value)
+    {
+        var myLogSource = Logger.CreateLogSource("MyLogSource");
+        Logger.Sources.Add(myLogSource);
+        myLogSource.LogInfo("Test");
+        
+        
+        if (value is string value2)
+        {
+            PlayerPrefs.SetString(key, value2);
+        }
+        else
+        {
+            string value3 = JsonConvert.SerializeObject(value);
+            PlayerPrefs.SetString(key, value3);
+        }
+        PlayerPrefs.SetString("Test", "Balls");
+        __result = Task.CompletedTask;
+        return false;
+    }
+    //public static Task Save(string key, object value) { }
+}
---
+++ Plugin.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+namespace MoloneyCustomCardInjector;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+    
+    private Harmony _harmony;
+    private void Awake()
+    {
+        _harmony = new Harmony(MyPluginInfo.PLUGIN_NAME);
+        _harmony.PatchAll(typeof(Patches));
+        // Plugin startup logic
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        
+    }
+    
+}
